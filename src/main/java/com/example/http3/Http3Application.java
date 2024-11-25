violetagg/spring-http3

Initial implementation

src/main/java/com/example/http3/Http3Application.java
@@ -1,0 +1,34 @@
+package com.example.http3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+
+@SpringBootApplication
+public class Http3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Http3Application.class, args);
+	}
+
+	@Bean
+	WebClient webClient(WebClient.Builder builder) {
+		HttpClient client =
+				HttpClient.create()
+						// Configure HTTP/3 protocol
+						.protocol(HttpProtocol.HTTP3)
+						// Configure HTTP/3 settings
+						.http3Settings(spec -> spec.idleTimeout(Duration.ofSeconds(5))
+								.maxData(10_000_000)
+								.maxStreamDataBidirectionalLocal(1_000_000));
+
+		return builder.clientConnector(new ReactorClientHttpConnector(client)).build();
+	}
+
+}

src/main/java/com/example/http3/Http3Controller.java
@@ -1,0 +1,29 @@
+package com.example.http3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+class Http3Controller {
+
+	private final WebClient webClient;
+
+	Http3Controller(WebClient webClient) {
+		this.webClient = webClient;
+	}
+
+	@GetMapping("/hello")
+	String hello() {
+		return "Hello HTTP/3!";
+	}
+
+	@GetMapping("/remote")
+	Mono<String> remote() {
+		return webClient.get()
+				.uri("https://projectreactor.io/")
+				.retrieve()
+				.bodyToMono(String.class);
+	}
+}

linux-china/spring-webflux-http3

src/main/java/com/example/http3/Http3Application.java
@@ -1,0 +1,34 @@
+package com.example.http3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+
+@SpringBootApplication
+public class Http3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Http3Application.class, args);
+	}
+
+	@Bean
+	WebClient http3WebClient(WebClient.Builder builder) {
+		HttpClient client =
+				HttpClient.create()
+						// Configure HTTP/3 protocol
+						.protocol(HttpProtocol.HTTP3)
+						// Configure HTTP/3 settings
+						.http3Settings(spec -> spec.idleTimeout(Duration.ofSeconds(5))
+								.maxData(10_000_000)
+								.maxStreamDataBidirectionalLocal(1_000_000));
+
+		return builder.clientConnector(new ReactorClientHttpConnector(client)).build();
+	}
+
+}

src/main/java/com/example/http3/Http3Controller.java
@@ -1,0 +1,29 @@
+package com.example.http3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+class Http3Controller {
+
+	private final WebClient http3WebClient;
+
+	Http3Controller(WebClient http3WebClient) {
+		this.http3WebClient = http3WebClient;
+	}
+
+	@GetMapping("/hello")
+	String hello() {
+		return "Hello HTTP/3!";
+	}
+
+	@GetMapping("/remote")
+	Mono<String> remote() {
+		return http3WebClient.get()
+				.uri("https://projectreactor.io/")
+				.retrieve()
+				.bodyToMono(String.class);
+	}
+}